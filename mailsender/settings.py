648fecb8a7b1ee8295686c49dbd83286e95a1b87
Shoaib Ghulam <shoaibghulam@gmail.com>
2023-10-06 02:39:21 +0500

    Update settings.py
    changes

--- mailsender/settings.py
+++ mailsender/settings.py
@@ -129,8 +129,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'danish.aliraza@fuuast.edu.pk'
-EMAIL_HOST_PASSWORD = 'danishbahoot12'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 STATIC_URL = '/static/'
